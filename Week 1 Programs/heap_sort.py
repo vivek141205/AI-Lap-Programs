6d86a63c71feffb86cd15fbed51fb921514336bd
vivek141205 <vivekv.cs24@bmsce.ac.in>
2026-09-21 16:24:46 +0530

    Add files via upload

---
+++ Week 1 Programs/heap_sort.py
@@ -0,0 +1,29 @@
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+    return arr
+
+arr = [38, 27, 43, 3, 9, 82, 10]
+print(heap_sort(arr))
